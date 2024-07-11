62cebf6e0a975ed6df6482d1a7a308f2e6e361fc
조성원 <tjddnjsjo@kakao.com>
2024-07-11 21:53:01 +0900

    refactor: update layout component with Navbar and Footer
    The code changes in the `src/routes/+layout.svelte` file update the layout component by adding the `Navbar` and `Footer` components. This improves the overall structure and functionality of the layout. The `Navbar` component provides site navigation and the `Footer` component displays copyright information and a link to GitHub.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`refactor` for code restructuring). It provides a clear and concise description of the purpose of the code changes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
 	import '../app.css';
+	import Footer from '../components/Footer.svelte';
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
-<slot />
+<div class="relative flex min-h-dvh flex-col">
+	<Navbar />
+	<div class="flex-1 p-4">
+		<slot />
+	</div>
+	<Footer />
+</div>
