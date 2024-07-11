04c268a25b240331e1154fb35e6753346c117e38
조성원 <tjddnjsjo@kakao.com>
2024-07-11 21:52:09 +0900

    feat: Add Footer and Navbar components
    - Added Footer component with copyright information and GitHub link
    - Added Navbar component with site name and basic structure

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Github from '$lib/icons/Github.svelte';
+</script>
+
+<footer class="footer bg-neutral text-neutral-content items-center p-4">
+	<aside class="grid-flow-col items-center">
+		<p>
+			Copyright {new Date().getFullYear()}.
+			<a href="https://github.com/wontory" class="hover:text-white">wontory</a> All right reserved
+		</p>
+	</aside>
+	<nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+		<a href="https://github.com/wontory/auto-table">
+			<Github class="h-6 w-6 transition-colors duration-200 hover:text-white" />
+		</a>
+	</nav>
+</footer>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<div class="navbar bg-base-100 sticky top-0">
+	<div class="navbar-start"></div>
+	<div class="navbar-center">
+		<a class="text-xl font-semibold" href="/">Auto Table</a>
+	</div>
+	<div class="navbar-end"></div>
+</div>
