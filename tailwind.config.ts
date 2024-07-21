6dd459c9616803c321d71f0956266dd94c88c3fd
조성원 <tjddnjsjo@kakao.com>
2024-07-22 05:17:51 +0900

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@ import daisyui from 'daisyui'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/contexts/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   plugins: [daisyui],
 }
 export default config
