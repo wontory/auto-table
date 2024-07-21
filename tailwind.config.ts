6b7111d1988024afe54b34fc2be79e2dc9428b24
조성원 <tjddnjsjo@kakao.com>
2024-07-21 22:49:02 +0900

    chore: remove unused tailwind config options

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	plugins: [],
 };
 export default config;
