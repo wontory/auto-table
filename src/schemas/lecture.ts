f4125de0a4b32c3333802f59b34949361def38ac
조성원 <tjddnjsjo@kakao.com>
2024-07-22 07:04:24 +0900

    feat: add lecture form error messages

--- src/schemas/lecture.ts
+++ src/schemas/lecture.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod'
 
 export const lectureSchema = z.object({
-  professor: z.string(),
-  day: z.enum(['MON', 'TUE', 'WED', 'THU', 'FRI']),
-  time: z.string(),
+  index: z.number(),
+  professor: z.string().min(1, { message: '담당교수를 입력해주세요.' }),
+  day: z.enum(['월', '화', '수', '목', '금'], {
+    message: '요일은 월, 화, 수, 목, 금 중 하나여야 합니다.',
+  }),
+  time: z.string().min(1, { message: '교시를 입력해주세요.' }),
 })
 
 export type Lecture = z.infer<typeof lectureSchema>
