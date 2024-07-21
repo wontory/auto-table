16576d2bc89ea0411cc4a6b3deabed4b23e6fcdf
조성원 <tjddnjsjo@kakao.com>
2024-07-22 05:23:09 +0900

    feat: add subject-list & add-subject-button into main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { AddSubjectButton } from '~/components/add-subject-button'
+import { SubjectList } from '~/components/subject-list'
+
 export default function Home() {
-  return
+  return (
+    <>
+      <SubjectList className="mb-4" />
+      <AddSubjectButton />
+    </>
+  )
 }
