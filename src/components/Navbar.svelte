c53d874d7206d4e76722e493d2c83d8a64d1ba7f
조성원 <tjddnjsjo@kakao.com>
2024-07-21 22:34:40 +0900

    chore: delete project

--- src/components/Navbar.svelte
+++
@@ -1,7 +0,0 @@
-<div class="navbar sticky top-0 bg-base-100">
-	<div class="navbar-start"></div>
-	<div class="navbar-center">
-		<a class="text-xl font-semibold" href="/">Auto Table</a>
-	</div>
-	<div class="navbar-end"></div>
-</div>
