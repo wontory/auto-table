6b1642c1b46213439d5e93125cf1d5011997ce17
조성원 <tjddnjsjo@kakao.com>
2024-07-22 00:19:19 +0900

    feat: add unsafe option to sort Tailwind CSS classes automatically

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 import path from 'node:path'
 
 const buildBiomeCommand = (filenames) =>
-  `biome check --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
+  `biome check --write --unsafe ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
 export default {
   '*.{js,jsx,ts,tsx,md,html,css}': [buildBiomeCommand],
