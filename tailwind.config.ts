d61379e7f0f80f00d558d37b97ba57ab6a833136
조성원 <tjddnjsjo@kakao.com>
2024-07-21 22:59:31 +0900

    style: format code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-	content: [
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	plugins: [],
-};
-export default config;
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  plugins: [],
+}
+export default config
