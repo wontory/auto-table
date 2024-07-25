0fb08c06df3dcbacaa42945426442f4a2d3079dc
조성원 <tjddnjsjo@kakao.com>
2024-07-26 05:03:05 +0900

    refactor: change collapse component to details & summary html tag

--- src/components/subject-list.tsx
+++ src/components/subject-list.tsx
@@ -26,35 +26,36 @@ export function SubjectList({ className }: { className?: string }) {
     subjectList.length > 0 && (
       <div className={cn('flex flex-col gap-4', className)}>
         {subjectList.map((subject) => (
-          <div key={subject.index} className="collapse-arrow collapse bg-base-200">
-            <input type="checkbox" />
-            <div className="collapse-title flex items-center justify-between gap-4 font-medium text-xl">
-              <div className="flex items-center gap-4">
-                <div className="badge badge-primary badge-lg aspect-square">{subject.credit}</div>
-                <span className="line-clamp-1">{subject.title}</span>
+          <details key={subject.index} className="collapse-arrow collapse bg-base-200">
+            <summary className="collapse-title">
+              <div className="flex items-center justify-between gap-4 font-medium text-xl">
+                <div className="flex items-center gap-4">
+                  <div className="badge badge-primary badge-lg aspect-square">{subject.credit}</div>
+                  <span className="line-clamp-1">{subject.title}</span>
+                </div>
+                <div className="z-50 flex gap-2">
+                  <button
+                    type="button"
+                    className="btn btn-square btn-outline btn-sm btn-success bg-base-100"
+                    onClick={() => handleEdit(subject)}
+                  >
+                    <EditIcon className="h-4 w-4" />
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-square btn-outline btn-sm btn-error bg-base-100"
+                    onClick={() => handleDelete(subject)}
+                  >
+                    <Trash2Icon className="h-4 w-4" />
+                  </button>
+                </div>
               </div>
-              <div className="z-50 flex gap-2">
-                <button
-                  type="button"
-                  className="btn btn-square btn-outline btn-sm btn-success bg-base-100"
-                  onClick={() => handleEdit(subject)}
-                >
-                  <EditIcon className="h-4 w-4" />
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-square btn-outline btn-sm btn-error bg-base-100"
-                  onClick={() => handleDelete(subject)}
-                >
-                  <Trash2Icon className="h-4 w-4" />
-                </button>
-              </div>
-            </div>
+            </summary>
             <div className="collapse-content">
               <LectureList subject={subject} className="mb-4" />
               <AddLectureButton subject={subject} />
             </div>
-          </div>
+          </details>
         ))}
       </div>
     )
