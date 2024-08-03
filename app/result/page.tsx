b6348fbbc88e5c7f626463de8270a43b22be86c8
조성원 <tjddnjsjo@kakao.com>
2024-08-04 03:12:53 +0900

    feat: sort timetables by score in Result component

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -10,13 +10,16 @@ export default function Result() {
 
   return timetables.length ? (
     <div className="grid gap-4 lg:grid-cols-2">
-      {timetables.map((timetable) => (
-        <div key={`timetable-${timetable.index}`} className="card bg-base-200">
-          <div className="card-body">
-            <Table timetable={timetable} />
+      {timetables
+        .sort((a, b) => b.score - a.score)
+        .map((timetable, index) => (
+          <div key={`timetable-${timetable.index}`} className="card bg-base-200">
+            <div className="card-body">
+              <h2 className="card-title">시간표 {index + 1}</h2>
+              <Table timetable={timetable} />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   ) : (
     <>생성된 시간표가 없어요.</>
