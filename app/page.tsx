4f1bdd17e79c4907b6c7b9cbf28245dee4c4b56a
조성원 <tjddnjsjo@kakao.com>
2024-08-04 01:03:18 +0900

    fix: update API endpoint in handleSubmit function

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   const setTimetables = useSetAtom(timetablesAtom)
 
   const handleSubmit = async () => {
-    const timetables = (await fetch('http://localhost:3000/api/combine', {
+    const timetables = (await fetch('/api/combine', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(subjects),
