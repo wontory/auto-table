19245f43542ddb988b504614c1508664c02c1b71
조성원 <tjddnjsjo@kakao.com>
2024-08-04 00:45:40 +0900

    feat: add combine route for timetable combination

---
+++ app/api/combine/route.ts
@@ -0,0 +1,49 @@
+import { days } from '~/constants/days'
+import type { Subject } from '~/schemas/subject'
+import type { Timetable } from '~/schemas/timetable'
+
+export async function POST(request: Request) {
+  const subjects = (await request.json()) as Subject[]
+  const combination: Subject[] = []
+  const timetables: Timetable[] = []
+
+  const combine = (depth = 0) => {
+    if (depth === subjects.length) {
+      const table: Timetable['timetable'] = Array.from({ length: 8 }, () => Array.from({ length: 5 }, () => null))
+
+      for (const subject of combination) {
+        if (subject.lectures[0].time) {
+          for (const time of Array.from(subject.lectures[0].time, (arg) => Number(arg) - 1)) {
+            table[time][days.indexOf(subject.lectures[0].day)] = subject
+          }
+        }
+      }
+
+      return timetables.push({ index: timetables.length, timetable: table })
+    }
+
+    for (const lecture of subjects[depth].lectures) {
+      const isReserved = combination.some(
+        (subject) =>
+          lecture.day === subject.lectures[0].day &&
+          (lecture.time.includes(subject.lectures[0].time.charAt(0)) ||
+            subject.lectures[0].time.includes(lecture.time.charAt(0))),
+      )
+
+      if (!isReserved) {
+        combination[depth] = {
+          ...subjects[depth],
+          lectures: [lecture],
+        }
+
+        combine(depth + 1)
+
+        combination.pop()
+      }
+    }
+  }
+
+  combine()
+
+  return Response.json(timetables)
+}
