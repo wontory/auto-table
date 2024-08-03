9018a034fdebf114009e47e77f8f40a22e8368cf
조성원 <tjddnjsjo@kakao.com>
2024-08-04 04:22:58 +0900

    feat: add back button

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,27 +1,47 @@
 'use client'
 
 import { useAtomValue } from 'jotai'
+import { Undo2Icon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 import { timetablesAtom } from '~/atoms/timetables'
 import { Table } from '~/components/table'
 
 export default function Result() {
+  const router = useRouter()
+
   const timetables = useAtomValue(timetablesAtom)
 
-  return timetables.length ? (
-    <div className="grid gap-4 lg:grid-cols-2">
-      {timetables
-        .sort((a, b) => b.score - a.score)
-        .map((timetable, index) => (
-          <div key={`timetable-${timetable.index}`} className="card bg-base-200">
-            <div className="card-body">
-              <h2 className="card-title">시간표 {index + 1}</h2>
-              <Table timetable={timetable} />
-            </div>
-          </div>
-        ))}
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => {
+            router.back()
+          }}
+        >
+          <Undo2Icon className="h-6 w-6" />
+          뒤로가기
+        </button>
+      </div>
+      {timetables.length ? (
+        <div className="grid gap-4 lg:grid-cols-2">
+          {timetables
+            .sort((a, b) => b.score - a.score)
+            .map((timetable, index) => (
+              <div key={`timetable-${timetable.index}`} className="card bg-base-200">
+                <div className="card-body">
+                  <h2 className="card-title">시간표 {index + 1}</h2>
+                  <Table timetable={timetable} />
+                </div>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <>생성된 시간표가 없어요.</>
+      )}
     </div>
-  ) : (
-    <>생성된 시간표가 없어요.</>
   )
 }
